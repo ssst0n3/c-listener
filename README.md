e0fd25030f3d5b8d4e20ffb46cde27a7079634e0
LEI WANG <ssst0n3@gmail.com>
2024-01-31 19:57:51 +0800

    init
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+
+```
+./listen -a allow_cmd1 [-a allow_cmd2] -d deny_cmd1 [-d deny_cmd2]
+```
+
+```
+./listen -a runc -a init -d root
+```
---
+++ listen.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type Listener struct {
+	self    string
+	PidList chan int
+	Store   sync.Map
+}
+
+func New() *Listener {
+	return &Listener{
+		self:    os.Args[0],
+		PidList: make(chan int, 10),
+	}
+}
+
+func valid(cmdline string, allows, denys []string) bool {
+	for _, target := range allows {
+		if !strings.Contains(cmdline, target) {
+			return false
+		}
+	}
+	for _, deny := range denys {
+		if strings.Contains(cmdline, deny) {
+			return false
+		}
+	}
+	return true
+}
+
+func (l *Listener) Listen(passSelf bool, allows []string, denys []string) {
+	for {
+		dirs, err := os.ReadDir("/proc")
+		if err != nil {
+			panic(err)
+		}
+		for _, dir := range dirs {
+			pid, err := strconv.ParseInt(dir.Name(), 10, 32)
+			if err != nil {
+				continue
+			}
+
+			//exe, err := filepath.EvalSymlinks(fmt.Sprintf("/proc/%d/exe", pid))
+			//if err != nil {
+			//	continue
+			//}
+			content, err := os.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
+			if err != nil {
+				continue
+			}
+			cmdline := string(content)
+			if passSelf && strings.Contains(cmdline, l.self) {
+				continue
+			}
+			if !valid(cmdline, allows, denys) {
+				continue
+			}
+
+			if _, ok := l.Store.Load(int(pid)); !ok {
+				fmt.Printf("[+] Found the PID: %d, %s\n", pid, cmdline)
+			}
+			l.PidList <- int(pid)
+		}
+	}
+}
+
+func (l *Listener) listFd(pid int) {
+	store := &sync.Map{}
+	l.Store.LoadOrStore(pid, store)
+	fds, err := os.ReadDir(fmt.Sprintf("/proc/%d/fd", pid))
+	if err != nil {
+		fmt.Printf("open /proc/%d/fd failed\n", pid)
+		return
+	}
+	changed := false
+	for _, name := range fds {
+		fd, err := strconv.Atoi(name.Name())
+		if err != nil {
+			continue
+		}
+		path, _ := os.Readlink(fmt.Sprintf("/proc/%d/fd/%d", pid, fd))
+		if path == "" {
+			path = "?"
+		}
+
+		if old, ok := store.Load(fd); !ok {
+			store.Store(pid, path)
+			changed = true
+		} else {
+			if old != path && path != "?" {
+				changed = true
+				store.Store(pid, path)
+			}
+		}
+	}
+	if changed {
+		var keys []int
+		store.Range(func(key any, value any) bool {
+			fd := key.(int)
+			keys = append(keys, fd)
+			return true
+		})
+		sort.Ints(keys)
+		for _, fd := range keys {
+			path, _ := store.Load(fd)
+			fmt.Printf("/proc/%d/fd/%d -> %s\n", pid, fd, path)
+		}
+	}
+}
+
+func main() {
+	app := &cli.App{
+		Name: "fd-listener",
+		Flags: []cli.Flag{
+			&cli.StringSliceFlag{
+				Name:    "allows",
+				Aliases: []string{"a"},
+			},
+			&cli.StringSliceFlag{
+				Name:    "denys",
+				Aliases: []string{"d"},
+			},
+		},
+		Action: func(context *cli.Context) error {
+			l := New()
+			go l.Listen(true, context.StringSlice("allows"), context.StringSlice("denys"))
+			for {
+				pid := <-l.PidList
+				l.listFd(pid)
+			}
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+}
